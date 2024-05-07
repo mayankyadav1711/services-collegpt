99fb7be1924c3e1b654bc3f33e4283f7f22be174
mayankyadav1711 <mykyadav2003@gmail.com>
2024-05-07 21:20:15 +0530

    contact us submitted

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,34 +1,63 @@
-import NewsLatterBox from "./NewsLatterBox";
+import { useState } from "react";
+import toast from 'react-hot-toast';
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('https://api-collegpt.vercel.app/service-contact', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      if (response.ok) {
+        console.log('Form submitted successfully');
+        toast.success("Will contact you shortly!");
+      } else {
+        console.error('Form submission failed');
+        toast.error("Something went wrong");
+      }
+    } catch (error) {
+      console.error('Error submitting form:', error);
+    }
+  };
+
   return (
     <section id="contact" className="overflow-hidden py-16 md:py-20 lg:py-28">
       <div className="container">
-        <div className="-mx-4 flex flex-wrap">  
+        <div className="-mx-4 flex flex-wrap">
           <div className="w-full px-4 ">
-            <div
-              className="mb-12 rounded-sm bg-white px-8 py-11 shadow-three dark:bg-[#000000] sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]"
-              data-wow-delay=".15s
-              "
-            >
+            <div className="mb-12 rounded-sm bg-white px-8 py-11 shadow-three dark:bg-[#000000] sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]" data-wow-delay=".15s">
               <h2 className="mb-3 text-2xl text-center font-bold text-black dark:text-white sm:text-3xl lg:text-2xl xl:text-3xl">
                 Contact Us
               </h2>
               <p className="mb-12 text-center text-base font-medium text-body-color">
                 Our support team will get back to you ASAP via email.
               </p>
-              <form>
+              <form onSubmit={handleSubmit}>
                 <div className="-mx-4 flex flex-wrap">
                   <div className="w-full px-4 md:w-1/2">
                     <div className="mb-8">
-                      <label
-                        htmlFor="name"
-                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
-                      >
+                      <label htmlFor="name" className="mb-3 block text-sm font-medium text-dark dark:text-white">
                         Your Name
                       </label>
                       <input
                         type="text"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
                         placeholder="Enter your name"
                         className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-white dark:border-transparent dark:bg-[#3f3f3f73] dark:text-body-color-dark dark:shadow-two dark:focus:border-white dark:focus:shadow-none"
                       />
@@ -36,14 +65,14 @@ const Contact = () => {
                   </div>
                   <div className="w-full px-4 md:w-1/2">
                     <div className="mb-8">
-                      <label
-                        htmlFor="email"
-                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
-                      >
+                      <label htmlFor="email" className="mb-3 block text-sm font-medium text-dark dark:text-white">
                         Your Email
                       </label>
                       <input
                         type="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
                         placeholder="Enter your email"
                         className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-white dark:border-transparent dark:bg-[#3f3f3f73] dark:text-body-color-dark dark:shadow-two dark:focus:border-white dark:focus:shadow-none"
                       />
@@ -51,14 +80,13 @@ const Contact = () => {
                   </div>
                   <div className="w-full px-4">
                     <div className="mb-8">
-                      <label
-                        htmlFor="message"
-                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
-                      >
+                      <label htmlFor="message" className="mb-3 block text-sm font-medium text-dark dark:text-white">
                         Your Message
                       </label>
                       <textarea
                         name="message"
+                        value={formData.message}
+                        onChange={handleChange}
                         rows={5}
                         placeholder="Enter your Message"
                         className="border-stroke w-full resize-none rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-white dark:border-transparent dark:bg-[#3f3f3f73] dark:text-body-color-dark dark:shadow-two dark:focus:border-white dark:focus:shadow-none"
@@ -66,7 +94,7 @@ const Contact = () => {
                     </div>
                   </div>
                   <div className="w-full px-4">
-                    <button className="rounded-sm bg-black dark:bg-white px-4 py-3 text-base font-medium dark:text-black text-white shadow-submit duration-300 hover:bg-white/90 dark:shadow-submit-dark">
+                    <button type="submit" className="rounded-sm bg-black dark:bg-white px-4 py-3 text-base font-medium dark:text-black text-white shadow-submit duration-300 hover:bg-white/90 dark:shadow-submit-dark">
                       Submit
                     </button>
                   </div>
@@ -74,7 +102,6 @@ const Contact = () => {
               </form>
             </div>
           </div>
-          
         </div>
       </div>
     </section>
